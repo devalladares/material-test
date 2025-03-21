4f065d422adf34677d4d694dcb1027c49055b15b
Trudy Painter <trudyp@google.com>
2025-03-21 19:35:44 -0400

    Add .gitignore to exclude node_modules and other build artifacts

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages/index.js -> ../components/CanvasContainer\":{\"id\":\"pages/index.js -> ../components/CanvasContainer\",\"files\":[\"static/chunks/_pages-dir-browser_components_CanvasContainer_js.js\"]}}"
